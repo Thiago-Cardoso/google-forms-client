8939e9aa26fad80dc3218308cb7ca0f799b791f4
Thiago-Cardoso <trovador182@gmail.com>
2018-03-31 21:16:40 -0300

    generate of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,28 @@ import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from './forms/forms.module';
+import { AnswersModule } from './answers/answers.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule,
     ColorPickerModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule,
+    AnswersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
