bedef1a097657ae16a5b8cb73b7b0c386c1b4ae5
Thiago-Cardoso <trovador182@gmail.com>
2018-04-10 20:52:44 -0300

    component of list form

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,24 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
 import { FormEditComponent } from './form-edit/form-edit.component';
-import { ActiveComponent } from './shared/active/active.component';
 import { MaterializeModule } from 'ng2-materialize';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { routing } from './../app.routing';
 import { FormsModule as CoreFormsModule } from '@angular/forms';
 
+
+
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    MaterializeModule.forRoot(),
+    MaterializeModule,
     routing,
     CoreFormsModule
   ],
   exports: [
     FormListComponent
   ],
-  declarations: [FormListComponent, FormEditComponent, ActiveComponent]
+  declarations: [FormListComponent, FormEditComponent]
 })
 export class FormsModule { }
--- src/app/shared/questions_answer.model.ts
+++ src/app/shared/questions_answer.model.ts
@@ -1,11 +1,11 @@
 import { Question } from './question.model';
- 
+
 export class QuestionsAnswer {
     id: number;
     value: string;
     answer_id: number;
     question: Question;
- 
+
     constructor(questionsAnswerInfo: any) {
         this.id = questionsAnswerInfo.id;
         this.value = questionsAnswerInfo.value;
